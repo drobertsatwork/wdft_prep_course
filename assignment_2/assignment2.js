db982806d101c3e42eb5be58af57f885346544b4
Mr. D Roberts <34330393+drobertsatwork@users.noreply.github.com>
2018-01-04 06:50:04 -0500

    Update assignment2.js

--- assignment_2/assignment2.js
+++ assignment_2/assignment2.js
@@ -19,3 +19,25 @@ Write your explanation for Exercise #1 here
 Write your explanation for Exercise #2 here
 
 */
+var nums = [1,2,34,54,55,34,32,11,19,17,54,66,13];
+var evens = [];
+var odds = [];
+
+var evenNumbers = function(nums) {
+    for (var i = 0; i < nums.length; i++) {
+
+        if ((nums[i] % 2) != 1) {
+            evens.push(nums[i]);
+                console.log(evens);
+        }
+        else {
+            odds.push(nums[i]);
+                console.log(odds);
+        }
+    }
+
+};
+
+evenNumbers(nums);
+alert(evens);
+alert(odds);
